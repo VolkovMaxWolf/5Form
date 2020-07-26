d70258c2d55248a8626d281a2237b8b3b8ee8207
VolkovMaxWolf <68462998+VolkovMaxWolf@users.noreply.github.com>
2020-07-26 11:26:09 +0300

    Add files via upload

---
+++ 5Form/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FindFuncButton(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && Сheck(textBox1.Text) &&
+                textBox2.Text != "" && Сheck(textBox2.Text) &&
+                textBox3.Text != "" && Сheck(textBox3.Text))
+            {
+                textBox4.Clear();
+                for (double i = Convert.ToDouble(textBox1.Text); i <= Convert.ToDouble(textBox2.Text); i += Convert.ToDouble(textBox3.Text))
+                    try
+                {
+                    textBox4.AppendText("y("+i+")="+f(i) + Environment.NewLine);
+                }
+                catch
+                {
+                    textBox4.AppendText("y(" + i + ")=error" + Environment.NewLine);
+                }
+            }
+        }
+        private bool Сheck(String s)
+        {
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!char.IsDigit(arr[i]) && arr[i] != '-' && arr[i] != ',')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static double f(double x)
+        {
+            try
+            {
+                if (x * x - 2 == 0 || x * x * x - 1 < 0) throw new Exception();
+                else return (x + 4) / (x * x - 2) + Math.Sqrt(x * x * x - 1);
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+    }
+}
---
+++ 5Form/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("5Form")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("5Form")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("aea87139-81e9-43dc-9a2a-7dac1840e383")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
